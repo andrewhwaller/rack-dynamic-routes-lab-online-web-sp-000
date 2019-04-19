accbab433b2e269b7b7294efe719b54d75560f5a
Andrew Waller <andrewhwaller@gmail.com>
2019-04-19 00:12:12 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,8 +8,10 @@ class Application
       item_name = req.path.split("/items/").last
       if item = @@items.find{|i| i.name == item_name}
       resp.write item.price
-      end
       else
+        resp.status = 404
+        resp.write "Item not found"
+      end
     else
       resp.status = 404
       resp.write "Route not found"
