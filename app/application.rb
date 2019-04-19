fe8bee002ff46f25e7681065eb29e60bc69c16b4
Andrew Waller <andrewhwaller@gmail.com>
2019-04-19 00:12:32 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
       if item = @@items.find{|i| i.name == item_name}
       resp.write item.price
       else
-        resp.status = 404
+        resp.status = 400
         resp.write "Item not found"
       end
     else
