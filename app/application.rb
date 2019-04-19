e4c37a9baa1cef0d1f7454f99a6d428b93802668
Andrew Waller <andrewhwaller@gmail.com>
2019-04-19 00:04:45 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,17 @@
+class application
+
+  @@items = []
+  
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      
+      resp.write "#{item.price}"
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+  end
+end 
